d93bda50be0d3c758914f9bd5f4615765e2856cc
bakar31 <abubakar1808031@gmail.com>
2023-06-14 20:33:51 +0600

    ongoing problem

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -3,17 +3,3 @@ from collections import defaultdict
 nums = [-1,1,0,-3,3]
 mul_dict = []
 
-for i in nums:
-    new_nums = nums.copy()
-    new_nums.remove(i)
-    print(new_nums)
-
-    mul = 1
-    for j in new_nums:
-        mul = mul*j
-    print(mul)
-    # mul_dict[i] = mul
-    mul_dict.append(mul)
-
-print(mul_dict)
-# print(list(mul_dict.values()))
